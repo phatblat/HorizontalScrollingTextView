79c821835b706dde8aa89ee6296fa83ffb89796a
Ben Chatelain <benchatelain@gmail.com>
2014-10-12 14:08:29 -0600

    Initial Commit

---
+++ HorizontalScrollingTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HorizontalScrollingTextView
+//
+//  Created by Ben Chatelain on 10/12/14.
+//  Copyright (c) 2014 phatblat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HorizontalScrollingTextView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HorizontalScrollingTextView
+//
+//  Created by Ben Chatelain on 10/12/14.
+//  Copyright (c) 2014 phatblat. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
